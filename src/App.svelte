2fee7aa075d2dc04292c0894948d7c44f818f1c3
Wilian Martinez <wmartinez@q6cyber.com>
2024-06-17 19:59:39 -0600

    feat(setup): add flowbite

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,33 @@
-<h1 class="p-2 font-semibold">Welcome to strix project using tailwind</h1>
+<script lang="ts">
+  import { Alert, Button, Card } from 'flowbite-svelte';
+</script>
+
+<div class="container p-10">
+  <h1 class="p-2 font-semibold">Welcome to strix project using tailwind</h1>
+  <Alert color="blue">This is a flowbite component</Alert>
+
+  <div class="p-10">
+    <Button>Default</Button>
+    <Button color="alternative">Alternative</Button>
+    <Button color="dark">Dark</Button>
+    <Button color="light">Light</Button>
+    <Button color="blue">Blue</Button>
+    <Button color="green">Green</Button>
+    <Button color="red">Red</Button>
+    <Button color="yellow">Yellow</Button>
+    <Button color="purple">Purple</Button>
+  </div>
+  <div class="p-10">
+    <Card href="/cards">
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        Here are the biggest enterprise technology acquisitions of 2021 so far,
+        in reverse chronological order.
+      </p>
+    </Card>
+  </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin'
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [flowbite()],
+};
