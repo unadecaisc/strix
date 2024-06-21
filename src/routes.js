58de3013a772a099192a753bf2f9bdae3c14fb12
paul48avila <116925014+paul48avila@users.noreply.github.com>
2024-06-21 17:29:53 -0600

    feat(config): implement basic routing systemImplement basic routing s… (#5)
    * feat(config): implement basic routing systemImplement basic routing system using svelte-spa-router to enhance navigation.https://linear.app/wmartzh/issue/2Q24-12/fe-instalar-svelte-router
    
    * fix: reorganize file locations to English naming convention
    
    * fix: reorganize file locations

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import Configuraciones from "./lib/pages/configuraciones.svelte";
+import Home from "./lib/pages/Home.svelte";
+
+const routes = {
+    "/": Home,
+    "/configuraciones": Configuraciones,
+    };
+
+
+export default routes;
