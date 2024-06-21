674258791eea83a57801494728059ea7586e0e1f
Paul <paul.avila@unadeca.net>
2024-06-21 16:25:47 -0600

    feat(config): implement basic routing systemImplement basic routing system using svelte-spa-router to enhance navigation.https://linear.app/wmartzh/issue/2Q24-12/fe-instalar-svelte-router

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import Home from "./routes/Home.svelte";
+import Configuraciones from "./routes/configuraciones.svelte";
+
+const routes = {
+    "/": Home,
+    "/configuraciones": Configuraciones,
+    };
+
+
+export default routes;
