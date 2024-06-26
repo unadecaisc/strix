0da1dabab90c7632c427bb0162bbcc546b29cdfd
Paul <paul.avila@unadeca.net>
2024-06-26 13:32:29 -0600

    feat(protected-routes): add

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import Config from "./lib/pages/Configuration.svelte";
 import Home from "./lib/pages/Home.svelte";
+import Login from "./lib/pages/Login.svelte";
 
 export type RouterType = {
   path: string;
   component: any;
+  authRequired?: boolean;
 };
 
 const routes: RouterType[] = [
@@ -14,6 +16,12 @@ const routes: RouterType[] = [
   {
     path: "/configuraciones",
     component: Config,
+    authRequired: true,
+  },
+  {
+    path: "/login",
+    component: Login,
   },
 ];
+
 export default routes;
