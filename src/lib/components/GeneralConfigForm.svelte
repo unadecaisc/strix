01cdc2bbad477ae932690dc5c5ace21967bc937e
Wilian Martinez <wmartzh@gmail.com>
2024-07-04 16:29:02 -0600

    feat(general-settings): Add general settings form (#16)

---
+++ src/lib/components/GeneralConfigForm.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import { Input, Button, Label, Spinner } from "flowbite-svelte";
+  import { createEventDispatcher, onMount } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let disabled = true;
+  export let isLoading = false;
+  export let currentState: GlobalSetting | null = null;
+
+  $: buttonText = disabled ? "Editar" : "Guardar";
+
+  function handleSumbit() {
+    if (disabled) {
+      disabled = false;
+    } else {
+      disabled = true;
+      dispatch("update", currentState);
+    }
+  }
+</script>
+
+<form
+  on:submit|preventDefault={handleSumbit}
+  class="flex g-3 space-x-4 items-end"
+>
+  {#if !currentState || isLoading}
+    <div class="text-center">
+      <Spinner />
+    </div>
+  {:else}
+    <Label>
+      <span>Precio</span>
+      <Input
+        type="text"
+        bind:value={currentState.defaultPrice}
+        placeholder="Precio"
+        {disabled}
+      ></Input>
+    </Label>
+    <Label>
+      <span>Codigo de Estudiante</span>
+      <Input
+        type="text"
+        bind:value={currentState.studentsCode}
+        placeholder="Codigo de Estudiante"
+        {disabled}
+      ></Input>
+    </Label>
+    <Label>
+      <span>Codigo de Beca</span>
+      <Input
+        type="text"
+        bind:value={currentState.scolarshipCode}
+        placeholder="Codigo de Beca"
+        {disabled}
+      ></Input>
+    </Label>
+    <Label>
+      <span>Codigo de Diezmo</span>
+      <Input
+        type="text"
+        bind:value={currentState.tithCode}
+        placeholder="Codigo de Diezmo"
+        {disabled}
+      ></Input>
+    </Label>
+    <div class="flex align-bottom">
+      <Button type="submit" size="sm" color="blue">{buttonText}</Button>
+    </div>
+  {/if}
+</form>
--- src/lib/pages/configurations/General.svelte
+++ src/lib/pages/configurations/General.svelte
@@ -1,1 +1,54 @@
-<h1>General Configuration</h1>
+<script lang="ts">
+  import { Alert } from "flowbite-svelte";
+  import { onMount } from "svelte";
+  import { getConfig, updateConfig } from "../../services/config.service";
+  import GeneralConfigForm from "../../components/GeneralConfigForm.svelte";
+
+  let globalSetting: GlobalSetting | null = null;
+  let error: string | null = null;
+  let success: string | null = null;
+  let isLoading = false;
+
+  function reloadConfig() {
+    isLoading = true;
+    getConfig().then((res) => {
+      globalSetting = res ?? null;
+    });
+    isLoading = false;
+  }
+
+  onMount(async () => {
+    reloadConfig();
+  });
+
+  function handleUpdate(e: CustomEvent<GlobalSetting>) {
+    isLoading = true;
+    updateConfig(e.detail)
+      .then(() => {
+        reloadConfig();
+      })
+      .then(() => {
+        error = null;
+        success = "Configuracion actualizada";
+      })
+      .catch((err) => {
+        error = err.message;
+      });
+    isLoading = false;
+  }
+</script>
+
+<div class="py-3">
+  <h1 class="text-xl font-bold">Configuracion General</h1>
+</div>
+{#if error}
+  <Alert type="error" dismissable>{error}</Alert>
+{/if}
+{#if success}
+  <Alert type="success" dismissable>{success}</Alert>
+{/if}
+<GeneralConfigForm
+  currentState={globalSetting}
+  on:update={handleUpdate}
+  {isLoading}
+></GeneralConfigForm>
