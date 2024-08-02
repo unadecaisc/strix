1c984a43577b9a97ef8d4be4c174193292deb5f7
JakecorralesCespedes <132969599+JakecorralesCespedes@users.noreply.github.com>
2024-08-01 18:42:56 -0600

    Feature/2q24 feat: Add edit button to student table row and remove double click (#34)
    * refactor(settings): add the requests route and add the students screen
    
    * (feat): Add edit button to student table row and remove double click

--- src/lib/pages/Requests.svelte
+++ src/lib/pages/Requests.svelte
@@ -3,8 +3,8 @@
   import type { PageRequestsType } from "$lib/components/types";
   import Request from "./application/Request.svelte";
   import Students from "./application/Students.svelte";
-  import type { LinkType } from "../../stores/navigation.store";
 
+  import type { LinkType } from "../../stores/navigation.store";
   const link: LinkType = { title: "Solicitudes", path: "/solicitudes" };
   const Requests: PageRequestsType[] = [
     {
